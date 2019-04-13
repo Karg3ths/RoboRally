8452b21011210f96e39e01d5375aa57b62eb7d4c
Kargeths <fanistosner@seznam.cz>
2019-04-13 13:50:51 +0200

    Did someone say robots?

---
+++ src/main/Robot.java
@@ -0,0 +1,16 @@
+package main;
+
+import main.GameSettings;
+import tmxLoader.Tileset;
+import tmxLoader.RobotType;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+	public class Robot extends JPanel {
+
+	    private GameSettings game;
+	    private String playerName, aiName;
+	    private int[] coordinates;
+}
---
+++ src/tmxLoader/RobotType.java
@@ -0,0 +1,7 @@
+package tmxLoader;
+ /**
+  * list of all possible robot types (player + 3 AIs); 
+  */
+public enum RobotType {
+	  Player, AI, AI2, AI3
+}
